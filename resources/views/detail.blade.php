9a7d24530bc12d94d48619ed2684ee48ec317043
Selim SARAL <saral_selim@hotmail.com>
2021-01-09 16:58:30 +0300

    add pagination and home views

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <style>
+        body {
+            font-family: 'Nunito';
+        }
+    </style>
+</head>
+<body>
+<div class="container mt-4">
+    <div class="row">
+
+        <h3>Ürünler</h3>
+
+        <div class="col-md-12">
+            <ul>
+                @foreach($products as $product)
+                    <li>{{ $product->name }}</li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 m-auto">
+                    {!! $products->links('vendor.pagination.bootstrap-4')  !!}
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <style>
+        body {
+            font-family: 'Nunito';
+        }
+    </style>
+</head>
+<body>
+<div class="container mt-4">
+    <div class="row">
+
+        <h3>Kategoriler</h3>
+
+        <div class="col-md-12">
+            {!! $categories !!}
+        </div>
+
+    </div>
+</div>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
